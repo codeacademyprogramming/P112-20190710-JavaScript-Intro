9d357c7960b350a13ceb75bf3cf4a2ae166634a0
Sattar Amrahov <sattar@code.local>
2019-07-10 12:57:15 +0400

    files added

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # P112-20190710-JavaScript-Intro
+
+Faydalı linklər:
+
+- https://www.w3schools.com/js/default.asp
+- https://www.w3schools.com/js/js_datatypes.asp
+- http://javascript.info/
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript
---
+++ js/main.js
@@ -0,0 +1,90 @@
+"use strict";
+
+
+// console.log("hello");
+
+// let sum = 5+15;
+
+// document.write(sum);
+
+// window.alert("window alert");
+// alert("Alert");
+
+
+// var age = 19;
+// age = age + 5;
+// console.log(age);
+
+// age = 99;
+// console.log(age);
+
+// let name12;
+
+// name = "Sattar";
+// console.log(name);
+
+// name = "Aga";
+
+// document.getElementById("demo").innerHTML = "Anar";
+
+
+
+// let num1 = 10, 
+//     num2 = 15,
+//     num3 = 20;
+// const group = "P112";
+// console.log(group);
+
+// let admin, name;
+// name = "Jone";
+// admin = name;
+
+// alert(admin);
+
+// let Aga = "10";
+// console.log(Aga / 10);
+// console.log("Yaver" / "Yaver");
+
+// let a = "Hello", b = "World";
+// console.log( a + " afdssafdsf  " + b + "..." );
+// console.log( `${a}      ${b}...`);
+
+// let age = 33;
+
+// console.log(age);
+
+
+// if(age == null){
+//     alert("age is null");
+// }
+// console.log(age);
+
+
+// let name = "Ilya";
+
+// alert( `hello ${1}` ); //  Misir error
+// alert( `hello ${" asdsad "}` ); // Esger - error
+// alert( `hello ${name}` ); // Cavid - hello ilya
+
+
+let a = "5";
+let b = 5;
+
+console.log( a !== b );
+
+let age = 60;
+
+if (age >= 20){
+    console.log("age greater than 20");
+} else {
+    console.log("age less than 20");
+}
+
+age >= 20 ? console.log("age greater than 20") : console.log("age less than 20");
+
+
+if ( !(b > 3) ) {
+    console.log("true");
+} else {
+    console.log("false");
+}
